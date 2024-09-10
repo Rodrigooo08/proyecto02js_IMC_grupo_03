5b170fbf266e2a12fb71dc204e40c24b83297ccc
LIFabrego <165028180+LIFabrego@users.noreply.github.com>
2024-09-09 21:21:19 -0300

    condicion segun el IMC
    se agrega el codigo para la funcion Derterminar nivel de IMC

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function calcularIMC(altura,peso){
     return peso / (altura * altura);
 }
-
+function determinarNivel(resultado){
+    let nivel = '';
+    if (resultado < 18.5) {
+        nivel = 'bajo peso';
+    } else if (resultado >= 18.5 && resultado <= 24.9) {
+        nivel = 'peso saludable';
+    } else if (resultado >= 25 && resultado <= 29.9) {
+        nivel = 'sobrepeso';
+    } else {
+        nivel = 'obesidad';
+    }
+    return nivel;
+}
 function resolver() {
     // Obtener los valores del formulario
     let nombre = document.getElementById("valorNombre").value;
@@ -13,4 +25,8 @@ function resolver() {
     let imc = calcularIMC(altura,peso);
     console.log(imc);
 
+    // Determinar el nivel de peso
+    let nivelIMC = '';
+    nivelIMC = determinarNivel(imc);
+    console.log(nivelIMC);
 }
